387bdc4773c343dd0ab767a4f4dc68c73896af8b
Juan <hizkiajun@gmail.com>
2023-06-02 13:25:09 +0700

    refactor: remove global store, leverage internal state management (#7)

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,62 +1,55 @@
 'use client';
 
 import Loading from '@/app/result/loading.tsx';
-import { getResultList } from '@/app/ui/home/SearchService.ts';
-import { SearchState, useSearchStore } from '@/app/ui/home/SearchStore.ts';
+import { SearchFilterType } from '@/app/ui/home/SearchFilterType.ts';
+import { getSearchResultList } from '@/app/ui/home/SearchService.ts';
 import { SearchType } from '@/app/ui/home/SearchType.ts';
 import Button from '@/app/ui/shared/Button.tsx';
 import React, { Suspense, useEffect, useState } from 'react';
 
-export default async function Result(): Promise<React.JSX.Element> {
-  const [keyword, pageSize] = useSearchStore((state: SearchState) => [
-    state.keyword,
-    state.pageSize,
-  ]);
-  // const resultList: SearchType[] = await getResultList({
-  //   page: '1',
-  //   keyword,
-  //   pageSize,
-  // });
+export default async function Result({
+  searchParams,
+}: {
+  searchParams: SearchFilterType;
+}): Promise<React.JSX.Element> {
+  const [page, setPage] = useState('1');
+  const [searchResultList, setSearchResultList] = useState([] as SearchType[]);
 
-  const [res, setRes] = useState([] as SearchType[]);
-  // const xxx = await getResultList({
-  //   page: '1',
-  //   keyword,
-  //   pageSize,
-  // });
-  // setRes(xxx);
+  const handleLoadMore = (): void => {
+    setPage(`${+page + 1}`);
+  };
 
   useEffect(() => {
+    const { keyword, pageSize }: SearchFilterType = searchParams;
+
     if (keyword) {
-      const fetchResultList = async () => getResultList({
-        page: '1',
+      const fetchSearchResultList = async (): Promise<SearchType[]> => getSearchResultList({
         keyword,
+        page,
         pageSize,
       });
 
-      fetchResultList().then((resp) => {
-        setRes(resp);
-      });
+      fetchSearchResultList().then(
+        (incomingSearchResultList: SearchType[]): void => {
+          setSearchResultList((currentSearchResultList: SearchType[]) => [
+            ...currentSearchResultList,
+            ...incomingSearchResultList,
+          ]);
+        },
+      );
     }
-  }, [keyword, pageSize]);
-
-  // useEffect(() => {
-  //   if (xxx?.length > 0) {
-  //     // setRes(xxx);]
-  //     console.log('===xxx', xxx);
-  //   }
-  // }, [xxx]);
+  }, [page, searchParams]);
 
   return (
     <>
       <article>
         <Suspense fallback={<Loading />}>
-          {res.map((r: SearchType) => (
-            <p key={r.id}>{r.name}</p>
+          {searchResultList.map((searchResult: SearchType) => (
+            <p key={searchResult.id}>{searchResult.name}</p>
           ))}
         </Suspense>
       </article>
-      <Button action="/result" text="more" />
+      <Button action={handleLoadMore} text="more" />
     </>
   );
 }
