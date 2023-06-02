a26a9ec7acd8cc14313b9b54121f924f51788ab2
Juan <hizkiajun@gmail.com>
2023-06-02 12:33:41 +0700

    chore: wip fixing the double loading

--- src/app/result/loading.tsx
+++ src/app/result/loading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Loading(): React.JSX.Element {
-  return <p>Loading..........</p>;
+  return <p>Loading Result..........</p>;
 }
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -5,25 +5,55 @@ import { getResultList } from '@/app/ui/home/SearchService.ts';
 import { SearchState, useSearchStore } from '@/app/ui/home/SearchStore.ts';
 import { SearchType } from '@/app/ui/home/SearchType.ts';
 import Button from '@/app/ui/shared/Button.tsx';
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 
 export default async function Result(): Promise<React.JSX.Element> {
   const [keyword, pageSize] = useSearchStore((state: SearchState) => [
     state.keyword,
     state.pageSize,
   ]);
-  const resultList: SearchType[] = await getResultList({
-    page: '1',
-    keyword,
-    pageSize,
-  });
+  // const resultList: SearchType[] = await getResultList({
+  //   page: '1',
+  //   keyword,
+  //   pageSize,
+  // });
+
+  const [res, setRes] = useState([] as SearchType[]);
+  // const xxx = await getResultList({
+  //   page: '1',
+  //   keyword,
+  //   pageSize,
+  // });
+  // setRes(xxx);
+
+  useEffect(() => {
+    if (keyword) {
+      const fetchResultList = async () =>
+        getResultList({
+          page: '1',
+          keyword,
+          pageSize,
+        });
+
+      fetchResultList().then((resp) => {
+        setRes(resp);
+      });
+    }
+  }, [keyword, pageSize]);
+
+  // useEffect(() => {
+  //   if (xxx?.length > 0) {
+  //     // setRes(xxx);]
+  //     console.log('===xxx', xxx);
+  //   }
+  // }, [xxx]);
 
   return (
     <>
       <article>
         <Suspense fallback={<Loading />}>
-          {resultList.map((res: SearchType) => (
-            <p key={res.id}>{res.name}</p>
+          {res.map((r: SearchType) => (
+            <p key={r.id}>{r.name}</p>
           ))}
         </Suspense>
       </article>
