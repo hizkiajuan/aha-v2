e7fab7190ca47dc1b50a880323064ffd5ac267e8
Juan <hizkiajun@gmail.com>
2023-06-02 13:24:33 +0700

    refactor: remove global store, leverage internal state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,30 @@
 
 import Search from '@/app/ui/home/Search.tsx';
 import SearchConfig from '@/app/ui/home/SearchConfig.tsx';
-import { SearchState, useSearchStore } from '@/app/ui/home/SearchStore.ts';
 import Button from '@/app/ui/shared/Button.tsx';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Home(): React.JSX.Element {
-  const [pageSize, updateKeyword, updatePageSize] = useSearchStore(
-    (state: SearchState) => [
-      state.pageSize,
-      state.updateKeyword,
-      state.updatePageSize,
-    ],
-  );
+  const [keyword, setKeyword] = useState('');
+  const [pageSize, setPageSize] = useState('');
+
   const handleChangeKeyword = (e: { target: HTMLInputElement }): void => {
-    updateKeyword(e.target.value.trim());
+    setKeyword(e.target.value.trim());
   };
   const handleChangePageSize = (e: { target: HTMLInputElement }): void => {
-    updatePageSize(e.target.value);
+    setPageSize(e.target.value);
   };
 
-  const router = useRouter();
+  const router: AppRouterInstance = useRouter();
   const handleSearch = (): void => {
-    router.push('/result');
+    const searchParams: string = new URLSearchParams({
+      keyword,
+      pageSize,
+    }).toString();
+    const url: string = `/result?${searchParams}`;
+    router.push(url);
   };
 
   return (
--- src/app/ui/home/SearchService.ts
+++ src/app/ui/home/SearchService.ts
@@ -3,14 +3,14 @@ import { SearchType } from '@/app/ui/home/SearchType.ts';
 import { SearchApiResponse } from '@/common/contract.ts';
 import http from '@/common/http.ts';
 
-export async function getResultList(
+export async function getSearchResultList(
   params: SearchFilterType,
 ): Promise<SearchType[]> {
   const response: Response = await http.get('api/users/all', params);
 
   if (!response.ok) {
     // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch result list');
+    throw new Error('Failed to fetch search result list');
   }
 
   const { data }: SearchApiResponse = await response.json();
