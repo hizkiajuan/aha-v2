181f00885cbef21ed1940554bb32ba2ec8c61abb
Juan <hizkiajun@gmail.com>
2023-06-03 15:26:15 +0700

    feat: implement ui design tags page for mobile & desktop view

--- src/app/tag/layout.tsx
+++ src/app/tag/layout.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export default function TagLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section>
-      <h1>Tags</h1>
+    <section className="mt-[70px] flex grow flex-col sm:mt-0 sm:overflow-y-scroll">
+      <h1 className="block px-5 pb-6 pt-5 text-2xl leading-9 sm:px-8 sm:pt-[80px] sm:text-[30px] md:px-32 lg:px-[257px]">
+        Tags
+      </h1>
       {children}
     </section>
   );
--- src/app/tag/page.tsx
+++ src/app/tag/page.tsx
@@ -7,14 +7,26 @@ export default async function Tag(): Promise<React.JSX.Element> {
   const tagList: TagApiResponse[] = await getTagList();
 
   return (
-    <>
-      <article>
+    <div className="px-[25px] pb-6 sm:px-8 md:px-32 lg:px-[257px]">
+      <article className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
         <Suspense fallback={<Loading />}>
-          {tagList?.map((tag: TagApiResponse) => (
-            <p key={tag.id}>{tag.name}</p>
+          {tagList.map((tag: TagApiResponse) => (
+            <div key={tag.id} className="">
+              <div className="flex h-[150px] w-[150px] items-end rounded-[10px] bg-aha-black-200 px-[10px] py-[14px]">
+                <p className="inline-block h-[50px] w-auto items-center justify-start truncate rounded-lg border-4 border-white px-[14px] py-[7px] text-2xl font-bold">
+                  {tag.name}
+                </p>
+              </div>
+              <h2 className="mt-5 truncate leading-[22px] tracking-[0.14px] text-[15h2x]">
+                {tag.name}
+              </h2>
+              <label className="text-[11px] leading-[17px] tracking-[0.37px] text-aha-grey-400">
+                {tag.count} Questions
+              </label>
+            </div>
           ))}
         </Suspense>
       </article>
-    </>
+    </div>
   );
 }
