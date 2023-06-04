87b2c92b22048f8d8c754c8aadde68b64ffbf82a
Juan <hizkiajun@gmail.com>
2023-06-05 00:23:28 +0700

    refine: content config for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,8 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/common/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
